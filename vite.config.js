6e0add753d2155578102aeddfc738c9321773c80
oscarraichert <oscarraichert@hotmail.com>
2022-08-25 03:38:22 -0300

    build e deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { ghPages } from "vite-plugin-gh-pages";
 
 const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "dist");
 
 export default defineConfig({
+
+    plugins: [ghPages()],
 
     base: "/pokedex/",
     root: root,
